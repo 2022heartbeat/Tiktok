977ba54f9499f8dca76eb18a0e17e190f02a10d8
255doesnotexist <1293883574jcy@gmail.com>
2023-02-02 12:43:18 +0800

    simple demo

---
+++ controller/feed.go
@@ -0,0 +1,23 @@
+package controller
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type FeedResponse struct {
+	Response
+	VideoList []Video `json:"video_list,omitempty"`
+	NextTime  int64   `json:"next_time,omitempty"`
+}
+
+// Feed same demo video list for every request
+func Feed(c *gin.Context) {
+	c.JSON(http.StatusOK, FeedResponse{
+		Response:  Response{StatusCode: 0},
+		VideoList: DemoVideos,
+		NextTime:  time.Now().Unix(),
+	})
+}
